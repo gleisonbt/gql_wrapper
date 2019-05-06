c78a6dc3187d9066b1a0cf34a4d1b546aa6473d4
gleisonbt <brittobaptista93@gmail.com>
2019-05-06 18:19:53 -0300

    endpoint language definition

--- gitHubGrahQLAPI/testing.py
+++ gitHubGrahQLAPI/testing.py
@@ -1,54 +1,81 @@
 import json
 import requests
-from genson import SchemaBuilder
 
 token = '773e67ef75e7274d5eef5722b1e714cc6c6770cc' #gleison
 
+URL = 'https://api.github.com' 
 
 def restCall(query, token=token):
     r = requests.get(query, headers={'Authorization': 'Bearer '+ token})
     return r.json()
 
-data = restCall('https://api.github.com/users/facebook/repos?type=all&page=1&per_page=10&order=desc&sort=created_at')
+endpoints = [
+    {
+        'path':'/users/{username}/repos',
+        'parameters':[{'type':'string'},{'sort':'string'},{'direction':'string'}],
+        'example':'https://api.github.com/users/gleisonbt/repos'
+    },
+    {
+        'path':'/users/{username}/starred',
+        'parameters':[{'type':'string'},{'sort':'string'},{'direction':'string'}],
+        'example':'https://api.github.com/users/gleisonbt/starred'
+    }
+]
+
+# [
+#     'https://api.github.com/users/facebook/repos',
+#     'https://api.github.com/users/gleisonbt/starred'
+# ]
+
+def name_root_node(endpoints):
+    
 
 
-def parseTypes(key, type):
-    if type == str:
+def generate_schema(endpoints):
+    for endpoint in endpoints:
+        data = restCall(endpoint['example'])
+
+        json2graphql(data, "")
+
+#data = restCall('https://api.github.com/users/facebook/repos?type=all&page=1&per_page=10&order=desc&sort=created_at')
+
+
+def parseTypes(key, value):
+    type_class = type(value)
+
+    if type_class == str:
         return 'String'
-    elif type == int:
+    elif type_class == int:
         return 'Int'
-    elif str(type.__name__) == 'NoneType':
+    elif str(type_class.__name__) == 'NoneType':
         return 'String'
-    elif type == bool:
+    elif type_class == bool:
         return 'Boolean'
-    elif type == dict:
+    elif type_class == dict:
         return key[0].upper() + key[1:len(key)]
-    else:
-        return str(type)
+    elif type_class == list:
+        return '[' + parseTypes(key, value[0]) + ']'
+    
     
 
 def json2graphql(json, rootType):
-    schema = """
-    """
+    schema = ""
     if type(json) == list:
         data = json[0]
     else:
         data = json
+     
+    schema = schema + "type " + rootType + " {" + "\n"
     
-    print("######### " + rootType + " #############")
     for key in data.keys():
-        print(key + ": " + parseTypes(key, type(data[key])))
-        schema + key + ": " + parseTypes(key, type(data[key])) + '\n'
+        schema = schema + '\t' + key + ": " + parseTypes(key, data[key]) + '\n'
         if type(data[key]) == dict:
-            json2graphql(data[key], parseTypes(key, type(data[key])))
-    
-    print("########################################")
+            schema = json2graphql(data[key], parseTypes(key, data[key])) + schema
+
+    schema = schema + '}' + '\n'
     return schema
     
 
-#print("type " + rootType + " {")
-#print("}")
 
-schema = json2graphql(data, "userRepos")
-
+print(schema)
 
