91f7b9b486f11ff0fe194b88385477dbb574c626
gleisonbt <brittobaptista93@gmail.com>
2019-05-07 19:26:07 -0300

    new features

--- gitHubGrahQLAPI/testing.py
+++ gitHubGrahQLAPI/testing.py
@@ -1,5 +1,6 @@
 import json
 import requests
+import random
 
 token = '773e67ef75e7274d5eef5722b1e714cc6c6770cc' #gleison
 
@@ -27,17 +28,31 @@ endpoints = [
 #     'https://api.github.com/users/gleisonbt/starred'
 # ]
 
-def name_root_node(endpoints):
+def path_variables(endpoint):
+    path_variables = []
+    paths  = endpoint['path'].split('/')
+    for path in paths[1:len(paths)]:
+        if path[0] == '{':
+            path_variables.append(path)
+    return path_variables
+
+def name_root_node(endpoint):
+    paths = endpoint['path'].split('/')
+    parameters = path_variables(endpoint)
+    for path_variable in parameters:
+        paths.remove(path_variable)
+    del paths[0]
     
+    cont = 1
+    for path in paths[1:len(paths)]:
+        paths[cont] = path[0].upper() + path[1:len(path)]
+        cont = cont + 1
 
-
-def generate_schema(endpoints):
-    for endpoint in endpoints:
-        data = restCall(endpoint['example'])
-
-        json2graphql(data, "")
-
-#data = restCall('https://api.github.com/users/facebook/repos?type=all&page=1&per_page=10&order=desc&sort=created_at')
+    name_root_name = ''
+    for path in paths:
+        name_root_name = name_root_name + path
+    
+    return name_root_name
 
 
 def parseTypes(key, value):
@@ -55,8 +70,9 @@ def parseTypes(key, value):
         return key[0].upper() + key[1:len(key)]
     elif type_class == list:
         return '[' + parseTypes(key, value[0]) + ']'
-    
-    
+
+type_list = []
+
 
 def json2graphql(json, rootType):
     schema = ""
@@ -65,17 +81,31 @@ def json2graphql(json, rootType):
     else:
         data = json
      
+    if rootType in type_list:
+        rootType = rootType + '_' + str(type_list.count(rootType))
+
     schema = schema + "type " + rootType + " {" + "\n"
     
     for key in data.keys():
+
         schema = schema + '\t' + key + ": " + parseTypes(key, data[key]) + '\n'
         if type(data[key]) == dict:
             schema = json2graphql(data[key], parseTypes(key, data[key])) + schema
 
     schema = schema + '}' + '\n'
     return schema
-    
+   
 
+def generate_schema(endpoints):
+    schema = ""
+    for endpoint in endpoints:
+        data = restCall(endpoint['example'])
+        root_node = name_root_node(endpoint)
+        schema = schema + json2graphql(data, root_node)
+    
+    return schema
+
+schema = generate_schema(endpoints)
 
 print(schema)
 
